a3ae84e43c9405e16e60857c4a8ff236f7b54dd8
Michael <michael.guerrero@outlook.com>
2019-03-23 21:34:18 -0500

    created ContactDetailsActivity

---
+++ app/src/main/java/com/michaelguerrero/android/mycard/ContactDetailsActivity.kt
@@ -0,0 +1,60 @@
+package com.michaelguerrero.android.mycard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class ContactDetailsActivity : AppCompatActivity() {
+
+    val TAG = "LifeCycle"
+
+
+
+    // function that handles onCreate of activity lifecycle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_details)
+    }
+
+
+    // function that handles onStart of activity lifecycle
+    override fun onStart() {
+        Log.d(TAG, "${javaClass.simpleName} OnStart")
+        super.onStart()
+    }
+
+
+    // function that handles onResume of activity lifecycle
+    override fun onResume() {
+        Log.d(TAG, "${javaClass.simpleName} OnResume")
+        super.onResume()
+    }
+
+
+    // function that handles onRestart of activity lifecycle
+    override fun onRestart() {
+        Log.d(TAG, "${javaClass.simpleName} onRestart")
+        super.onRestart()
+    }
+
+
+    // function that handles onPause of activity lifecycle
+    override fun onPause() {
+        Log.d(TAG, "${javaClass.simpleName} onPause")
+        super.onPause()
+    }
+
+
+    // function that handles onStop of activity lifecycle
+    override fun onStop() {
+        Log.d(TAG, "${javaClass.simpleName} onStop")
+        super.onStop()
+    }
+
+
+    // function that handles onDestroy of activity lifecycle
+    override fun onDestroy() {
+        Log.d(TAG, "${javaClass.simpleName} onDestroy")
+        super.onDestroy()
+    }
+}
